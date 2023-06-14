300c6ef94f23eb25e94d4899e6582269f81c3c60
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2023-06-14 17:46:24 -0400

    in app.js added a new route for cartscreen, in index.css added a new class for thumbnail img, created a new screen called cartscreen to show list of items in the cart and a button to checkout, in productscreen used useNavigate from reactrouterdom to redirect used to cartscreen after adding a new item to cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,7 @@ import { useContext, useEffect, useReducer } from "react";
 import Col from "react-bootstrap/esm/Col";
 import Card from "react-bootstrap/esm/Card";
 import Row from "react-bootstrap/esm/Row";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Rating from "../components/Rating";
 import ListGroup from "react-bootstrap/esm/ListGroup";
 import Badge from "react-bootstrap/esm/Badge";
@@ -28,6 +28,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
 
@@ -64,6 +65,7 @@ function ProductScreen() {
       type: "CART_ADD_ITEM",
       payload: { ...product, quantity },
     });
+    navigate("/cart");
   };
 
   return loading ? (
